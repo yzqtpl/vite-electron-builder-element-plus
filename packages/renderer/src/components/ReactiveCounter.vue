61589928f06da1e14cdbfdfc992d13f2b6669572
yzqdev <yzqdev@outlook.com>
2022-01-10 20:13:49 +0800

    init comment

---
+++ packages/renderer/src/components/ReactiveCounter.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <el-button @click="count++">
+    count is: {{ count }}
+  </el-button>
+  <br><br>
+  <code>packages/renderer/src/components/ReactiveCounter.vue</code>
+</template>
